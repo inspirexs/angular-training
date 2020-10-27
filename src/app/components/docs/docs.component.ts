2cd0944ef39550311d4595d89868547c612b44d0
Lubomir Molcan <lmolcan@globalblue.com>
2020-10-27 07:48:51 +0100

    css

--- src/app/components/docs/docs.component.ts
+++ src/app/components/docs/docs.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-docs',
   templateUrl: './docs.component.html',
-  styleUrls: ['./docs.component.scss']
+  styleUrls: ['./docs.component.css']
 })
 export class DocsComponent implements OnInit {
 
